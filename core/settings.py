3e04d5fb4cf4922261f97925dcac9dd069cb4973
Ratana Nov <rayanratana@gmail.com>
2025-03-06 19:37:00 +0700

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -57,13 +57,13 @@ CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # Required for static files on Heroku
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'core.middlewares.CustomAMSMiddleware',
 ]
 
 ROOT_URLCONF = 'core.urls'

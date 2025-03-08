825dd118dbef6429275418a68488a464a4b414d3
Ratana Nov <rayanratana@gmail.com>
2025-03-08 15:48:48 +0700

    Delete all

--- accounts/email.py
+++
@@ -1,5 +0,0 @@
-from core.base.mail import BaseEmailMessage
-
-
-class UserInviteEmail(BaseEmailMessage):
-    template_name = "email/user_invite.html"
--- dashboard/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import AssetModel
-
-
-admin.site.register(AssetModel)

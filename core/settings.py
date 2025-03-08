e5977cde05ef5fa03723c97ef68b2a1ca8403927
Ratana Nov <rayanratana@gmail.com>
2025-03-08 22:45:13 +0700

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -18,8 +18,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY', "key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = False
-DEBUG = os.getenv('DEBUG', 'True') =='True'
+DEBUG = True
+# DEBUG = os.getenv('DEBUG', 'True') =='True'
 
 # ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
 # ALLOWED_HOSTS = 'courier-app-h2dh4.ondigitalocean.app'

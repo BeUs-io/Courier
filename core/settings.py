df84c4a3e2f5c99378aea617b25e80084a88011c
Ratana Nov <rayanratana@gmail.com>
2025-03-08 22:13:45 +0700

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -18,7 +18,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY', "key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+# DEBUG = False
+DEBUG = os.getenv('DEBUG', 'True') =='True'
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', "*").split(',')
 

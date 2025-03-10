615c8475deaa2226174dfdff35d432d8820bd639
Ratana Nov <rayanratana@gmail.com>
2025-03-10 09:32:39 +0700

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -15,11 +15,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY', "key")
+SECRET_KEY = os.getenv('SECRET_KEY', "u2_y3p1ny*wa)s=!fed1&iy70de3duuoqxn(9qy)kv+k_rciw8")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-# DEBUG = os.getenv('DEBUG', 'True') =='True'
+# DEBUG = True
+DEBUG = os.getenv('DEBUG', 'True') =='True'
 
 # ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
 ALLOWED_HOSTS = ['courier-app-h2dh4.ondigitalocean.app']
